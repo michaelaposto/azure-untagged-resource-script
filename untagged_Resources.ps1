815d3e7d2a2c2a5c741d46ebf3cfa2f84cd53883
aposto26 <michael.aposto26@gmail.com>
2023-02-06 13:39:10 +1100

    Updated script using array to store untagged resources

--- untagged_Resources.ps1
+++ untagged_Resources.ps1
@@ -1,8 +1,22 @@
-$resources = Get-AzureRmResource
-foreach($resource in $resources)
-{
-    if ($resource.Tags -eq $null)
-    {
-        echo $resource.Name, $resource.ResourceType
+# Script to list untagged resources
+
+# Create resource variable
+$resources = Get-AzResource | Where-Object {$null -eq $_.Tags -or $_.Tags.Count -eq 0}
+
+# Create results array which stores the untagged resources to be used in weekly email
+$results = @()
+
+# Loop to list the the untagged resources, resource type and the resource group
+foreach ($resource in $resources){
+    $results += New-Object PSObject -Property @{
+        Name   = $resource.Name
+        Type   = $resource.resourcetype 
+        RGName = $resource.resourcegroupname
     }
 }
+
+# Test to see if the array is functioning correctly
+Write-Output $results 
+
+# Convert array into a .csv and send in email to azenix members 
+$results | Export-Csv -Path "UntaggedResources.csv" -NoTypeInformation
